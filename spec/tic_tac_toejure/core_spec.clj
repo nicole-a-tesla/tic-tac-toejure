65e03df1f30591eb79f3d5009438b33634542634
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-03-31 15:30:28 -0400

    Show human player moves on board print
    * New board passed on each game-play recur, so change associated functions to take board as arg
    * Adjust spacing for better alignment on marker print
    * Presumptively convert user input strings to integers

--- spec/tic_tac_toejure/core_spec.clj
+++ spec/tic_tac_toejure/core_spec.clj
@@ -2,12 +2,17 @@
   (:require [speclj.core :refer :all]
             [tic-tac-toejure.core :refer :all]))
 
+(def empty-board (vec (repeat 9 "")))
+
+(def empty-board-output-str (str empty-space-view empty-space-view empty-space-view "\n"
+                                 empty-space-view empty-space-view empty-space-view "\n"
+                                 empty-space-view empty-space-view empty-space-view "\n"))
+
 (describe "Ui"
   (around [it]
     (with-out-str (it)))
 
   (describe "test prompt"
-
     (it "tests the input of prompt"
       (should= "X"
         (with-in-str "X"
@@ -16,22 +21,28 @@
     (it "tests the output of prompt"
       (should= "X or O?\n"
        (with-out-str (with-in-str "O"
-        (prompt "X or O?")))))
-  )
+        (prompt "X or O?"))))))
 
   (describe "test print-board"
     (it "prints board to stdout"
-
-      (should= (str space space space "\n"
-                    space space space "\n"
-                    space space space "\n")
-        (with-out-str (print-board))))
-  )
+      (should= empty-board-output-str
+        (with-out-str (print-board empty-board)))))
 
   (describe "prompt user for position choice"
     (it "test gets user input"
       (with-redefs [prompt (fn [& _] "0")]
         (should= "0"
-          (get-player-move))))
-  )
+          (get-player-move)))))
+
+  (describe "build-board-view"
+    (it "builds board representation for empty board"
+      (should= (repeat 9 ":___")
+      (build-view empty-board))))
+
+)
+
+(describe "place-marker"
+  (it "adds marker to space"
+    (should= ["X" "" "" "" "" "" "" "" ""]
+      (place-marker build-board "0" "X")))
 )
--- src/tic_tac_toejure/core.clj
+++ src/tic_tac_toejure/core.clj
@@ -1,12 +1,15 @@
 (ns tic-tac-toejure.core)
 
+; Constants & defs
+
 (def get-player-move-message "Select your next move...")
 
-(def space ":___")
+(def empty-space-view ":___")
 
-(def board
-  (get (vector (repeat 9 space)) 0)
-)
+(def build-board
+  (vec (repeat 9 "")))
+
+; Ui
 
 (defn print-it [message]
   (println (apply str message)))
@@ -15,25 +18,44 @@
   (print-it message)
   (read-line))
 
-(defn print-board
-  ([]
-  (print-board (partition 3 board)))
-  ([rows]
-    (print-it (first rows))
-    (if-not (empty? (rest rows))
-      (print-board (rest rows)))
-    )
-)
-
 (defn get-player-move []
   (prompt get-player-move-message)
 )
 
-(defn play []
-  (print-board)
+(defn state-to-view-mapper [space-contents]
+  (if (clojure.string/blank? space-contents)
+    empty-space-view
+    (str ": " space-contents " ")))
+
+(defn build-view [row]
+  (map state-to-view-mapper row))
+
+(defn print-board
+  ([board]
+  (print-board board (partition 3 board)))
+  ([board rows]
+    (print-it (build-view(first rows)))
+    (if-not (empty? (rest rows))
+      (recur board (rest rows)))
+    )
+)
+
+; move logic
+
+(defn as-int [input]
+  (read-string input))
+
+(defn place-marker [board position marker]
+  (assoc board (as-int position) marker))
+
+; Game loop
+
+(defn play [board]
+  (print-board board)
   (def move (get-player-move))
+  (def next-board (place-marker board move "X"))
   (if-not (= move "q")
-    (recur)))
+    (recur next-board)))
 
 (defn -main [& args]
-  (play))
+  (play build-board))

50bfbebacd48ff38b9713f7593bdeb17a7ac5494
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-03-31 15:56:46 -0400

    Separate Ui into own file

--- src/tic_tac_toejure/core.clj
+++ src/tic_tac_toejure/core.clj
@@ -1,46 +1,8 @@
-(ns tic-tac-toejure.core)
-
-; Constants & defs
-
-(def get-player-move-message "Select your next move...")
-
-(def empty-space-view ":___")
+(ns tic-tac-toejure.core
+  (:require [tic-tac-toejure.ui :refer :all]))
 
 (def build-board
   (vec (repeat 9 "")))
-
-; Ui
-
-(defn print-it [message]
-  (println (apply str message)))
-
-(defn prompt [message]
-  (print-it message)
-  (read-line))
-
-(defn get-player-move []
-  (prompt get-player-move-message)
-)
-
-(defn state-to-view-mapper [space-contents]
-  (if (clojure.string/blank? space-contents)
-    empty-space-view
-    (str ": " space-contents " ")))
-
-(defn build-view [row]
-  (map state-to-view-mapper row))
-
-(defn print-board
-  ([board]
-  (print-board board (partition 3 board)))
-  ([board rows]
-    (print-it (build-view(first rows)))
-    (if-not (empty? (rest rows))
-      (recur board (rest rows)))
-    )
-)
-
-; move logic
 
 (defn as-int [input]
   (read-string input))

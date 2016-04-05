c82ed6d4f04436a8b113fe594747a7a2343fe86b
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-04-05 14:04:34 -0400

    Improve Random AI's tests

--- spec/tic_tac_toejure/ai_spec.clj
+++ spec/tic_tac_toejure/ai_spec.clj
@@ -2,7 +2,11 @@
   (:require [speclj.core :refer :all]
             [tic-tac-toejure.ai :refer :all]))
 
-(describe "Ai"
+(describe "Random Ai"
   (describe "random-move"
     (it "returns a string"
-      (should= java.lang.String (type (random-move))))))
+      (should= java.lang.String (type (random-move))))
+
+    (it "returns a string that can be converted to an integer"
+      (should-not-throw
+        (Integer/parseInt (random-move))))))
